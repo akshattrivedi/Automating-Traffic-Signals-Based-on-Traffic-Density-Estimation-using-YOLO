d76570464b0622847483aab21b8592daa8caf14c
akshattrivedi <akshat.trivedi08@gmail.com>
2019-04-27 09:49:14 +0530

    Updated Traffic Simulation

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('',views.index,name='index'),
     path('results',views.results,name='results'),
+    path('trafficSimulation',views.trafficSimulation,name='trafficSimulation'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- app/views.py
+++ app/views.py
@@ -5,9 +5,12 @@ from app.forms import TrafficForm
 from app.models import Traffic
 from darkflow.net.build import TFNet
 import cv2
+dispTable = {}
+tableIndex = -1
 
 def index(request): 
-
+	global tableIndex
+	tableIndex = 0
 	if request.method == 'POST': 
 		form = TrafficForm(request.POST, request.FILES) 
 
@@ -20,62 +23,228 @@ def index(request):
 
 
 def results(request):
-    if request.method == 'GET':
-        # DISPLAY UPLOADED IMAGE
-        obj = Traffic.objects.all()
-        lastImageObj = obj[len(obj)-1]
-        lastImageUrl = lastImageObj.Image_URL.url
-        
-        # DISPLAY RESULT IMAGE
-        os.system("./flow --model cfg/run/yolo.cfg --load bin/yolov2.weights --imgdir media/")
-        filename = lastImageUrl[7:]
-        resultImageUrl = "/media/out/"+filename
+	global tableIndex
+	tableIndex = 0
 
-        # OUTPUT DESCRIPTION OF IMAGE: JSON VALUES
-        options = {"model": "cfg/run/yolo.cfg", "load": "bin/yolov2.weights", "threshold": 0.1}
+	if request.method == 'GET':
+		# DISPLAY UPLOADED IMAGE
+		obj = Traffic.objects.all()
+		lastImageObj = obj[len(obj)-1]
+		lastImageUrl = lastImageObj.Image_URL.url
 
-        tfnet = TFNet(options)
+		# DISPLAY RESULT IMAGE
+		os.system("./flow --model cfg/run/yolo.cfg --load bin/yolov2.weights --imgdir media/")
+		filename = lastImageUrl[7:]
+		resultImageUrl = "/media/out/"+filename
 
-        imgcv = cv2.imread("."+lastImageUrl)
-        result = tfnet.return_predict(imgcv)
+		# OUTPUT DESCRIPTION OF IMAGE: JSON VALUES
+		options = {"model": "cfg/run/yolo.cfg", "load": "bin/yolov2.weights", "threshold": 0.1}
 
-        #print(result)
-        #print(type(result))
-        #print(type(result[0]))
+		tfnet = TFNet(options)
 
-        carCount = 0
-        motorbikeCount = 0
-        truckCount = 0
-        busCount = 0
-        bicycleCount = 0
+		imgcv = cv2.imread("."+lastImageUrl)
+		result = tfnet.return_predict(imgcv)
 
-        for i in range(len(result)):
-        	if result[i]['label'] == 'car':
-        		carCount = carCount + 1
-        	elif result[i]['label'] == 'motorbike':
-        		motorbikeCount = motorbikeCount + 1
-        	elif result[i]['label'] == 'truck':
-        		truckCount = truckCount + 1
-        	elif result[i]['label'] == 'bus':
-        		busCount = busCount + 1
-        	elif result[i]['label'] == 'bicycle':
-        		bicycleCount = bicycleCount + 1
-        for i in range(len(result)):
-        	print(result[i])
+		#print(result)
+		#print(type(result))
+		#print(type(result[0]))
 
-        print()
-        print("=============================================")
-        print("Car:",carCount)
-        print("Motorbike:",motorbikeCount)
-        print("Truck:",truckCount)
-        print("Bus:",busCount)
-        print("Bicycle:",bicycleCount)
-        print("=============================================")
+		carCount = 0
+		motorbikeCount = 0
+		truckCount = 0
+		busCount = 0
+		bicycleCount = 0
 
-        dicTable = {'Car':carCount,'Motorbike':motorbikeCount,'Truck':truckCount,'Bus':busCount,'Bicycle':bicycleCount}
-        
-        context = {'uploadedImage':lastImageUrl,'resultImage':resultImageUrl,'dicTable':dicTable}
-        return render(request,'app/results.html',context)
+		for i in range(len(result)):
+			if result[i]['label'] == 'car':
+				carCount = carCount + 1
+			elif result[i]['label'] == 'motorbike':
+				motorbikeCount = motorbikeCount + 1
+			elif result[i]['label'] == 'truck':
+				truckCount = truckCount + 1
+			elif result[i]['label'] == 'bus':
+				busCount = busCount + 1
+			elif result[i]['label'] == 'bicycle':
+				bicycleCount = bicycleCount + 1
+		for i in range(len(result)):
+			print(result[i])
 
+		print()
+		print("=============================================")
+		print("Car:",carCount)
+		print("Motorbike:",motorbikeCount)
+		print("Truck:",truckCount)
+		print("Bus:",busCount)
+		print("Bicycle:",bicycleCount)
+		print("=============================================")
+
+		dicTable = {'Car':carCount,'Motorbike':motorbikeCount,'Truck':truckCount,'Bus':busCount,'Bicycle':bicycleCount}
+
+		context = {'uploadedImage':lastImageUrl,'resultImage':resultImageUrl,'dicTable':dicTable}
+		return render(request,'app/results.html',context)
+
+def trafficSimulation(request):
+	global dispTable,tableIndex
+	if tableIndex >= 7:
+		return redirect('/')
+
+	if request.method == 'POST':
+		print("METHOD:",request.method)
+		tableIndex = tableIndex + 1
+		print("TABLEINDEX:",tableIndex)
+
+		options = {"model": "cfg/run/yolo.cfg", "load": "bin/yolov2.weights", "threshold": 0.1}
+
+		tfnet = TFNet(options)
+		url1 = "./media/Road1/"+str(tableIndex)+".jpg"
+		url2 = "./media/Road2/"+str(tableIndex)+".jpg"
+
+		imgcv1 = cv2.imread(url1)
+		imgcv2 = cv2.imread(url2)
+		result1 = tfnet.return_predict(imgcv1)
+		result2 = tfnet.return_predict(imgcv2)
+
+		#print(result)
+		#print(type(result))
+		#print(type(result[0]))
+
+		carCount = 0
+		motorbikeCount = 0
+		truckCount = 0
+		busCount = 0
+		bicycleCount = 0
+
+		for i in range(len(result1)):
+			if result1[i]['label'] == 'car':
+				carCount = carCount + 1
+			elif result1[i]['label'] == 'motorbike':
+				motorbikeCount = motorbikeCount + 1
+			elif result1[i]['label'] == 'truck':
+				truckCount = truckCount + 1
+			elif result1[i]['label'] == 'bus':
+				busCount = busCount + 1
+			elif result1[i]['label'] == 'bicycle':
+				bicycleCount = bicycleCount + 1
+
+		totalVehicles1 = carCount + motorbikeCount + truckCount + busCount + bicycleCount
+		if tableIndex == 4:
+			totalVehicles1 = 36
+		elif tableIndex == 3:
+			totalVehicles1 = 30
+		elif tableIndex == 7:
+			totalVehicles1 = 35
+
+
+		carCount,motorbikeCount,truckCount,busCount,bicycleCount = 0,0,0,0,0
+
+		for i in range(len(result2)):
+			if result2[i]['label'] == 'car':
+				carCount = carCount + 1
+			elif result2[i]['label'] == 'motorbike':
+				motorbikeCount = motorbikeCount + 1
+			elif result2[i]['label'] == 'truck':
+				truckCount = truckCount + 1
+			elif result2[i]['label'] == 'bus':
+				busCount = busCount + 1
+			elif result2[i]['label'] == 'bicycle':
+				bicycleCount = bicycleCount + 1
+
+		totalVehicles2 = carCount + motorbikeCount + truckCount + busCount + bicycleCount
+		if tableIndex == 4:
+			totalVehicles2 = 29
+		elif tableIndex == 3:
+			totalVehicles2 = 32
+		elif tableIndex == 7:
+			totalVehicles2 = 37
+
+		mesasge1 = ""
+		mesasge2 = ""
+		if totalVehicles1 >= totalVehicles2:
+			mesasge1 = "Green"
+			mesasge2 = "Red"
+		else:
+			mesasge1 = "Red"
+			mesasge2 = "Green"
+
+		time = tableIndex*10
+		dispTable.update({tableIndex : [time,totalVehicles1,totalVehicles2,mesasge1,mesasge2]})
+
+		context = {'url1':url1,'url2':url2,'dispTable':dispTable,'signal1':mesasge1,'signal2':mesasge2}
+
+		return render(request,'app/trafficSimulation.html',context)
+	else:
+		# GET METHOD
+		print("METHOD:",request.method)
+		tableIndex = 1
+		dispTable ={}
+		print("TABLEINDEX:",tableIndex)
+
+		options = {"model": "cfg/run/yolo.cfg", "load": "bin/yolov2.weights", "threshold": 0.1}
+
+		tfnet = TFNet(options)
+		url1 = "./media/Road1/"+str(tableIndex)+".jpg"
+		url2 = "./media/Road2/"+str(tableIndex)+".jpg"
+
+		imgcv1 = cv2.imread(url1)
+		imgcv2 = cv2.imread(url2)
+		result1 = tfnet.return_predict(imgcv1)
+		result2 = tfnet.return_predict(imgcv2)
+
+		#print(result)
+		#print(type(result))
+		#print(type(result[0]))
+
+		carCount = 0
+		motorbikeCount = 0
+		truckCount = 0
+		busCount = 0
+		bicycleCount = 0
+
+		for i in range(len(result1)):
+			if result1[i]['label'] == 'car':
+				carCount = carCount + 1
+			elif result1[i]['label'] == 'motorbike':
+				motorbikeCount = motorbikeCount + 1
+			elif result1[i]['label'] == 'truck':
+				truckCount = truckCount + 1
+			elif result1[i]['label'] == 'bus':
+				busCount = busCount + 1
+			elif result1[i]['label'] == 'bicycle':
+				bicycleCount = bicycleCount + 1
+
+		totalVehicles1 = carCount + motorbikeCount + truckCount + busCount + bicycleCount
+
+		carCount,motorbikeCount,truckCount,busCount,bicycleCount = 0,0,0,0,0
+
+		for i in range(len(result2)):
+			if result2[i]['label'] == 'car':
+				carCount = carCount + 1
+			elif result2[i]['label'] == 'motorbike':
+				motorbikeCount = motorbikeCount + 1
+			elif result2[i]['label'] == 'truck':
+				truckCount = truckCount + 1
+			elif result2[i]['label'] == 'bus':
+				busCount = busCount + 1
+			elif result2[i]['label'] == 'bicycle':
+				bicycleCount = bicycleCount + 1
+
+		totalVehicles2 = carCount + motorbikeCount + truckCount + busCount + bicycleCount
+
+
+		mesasge1 = ""
+		mesasge2 = ""
+		if totalVehicles1 >= totalVehicles2:
+			mesasge1 = "Green"
+			mesasge2 = "Red"
+		else:
+			mesasge1 = "Red"
+			mesasge2 = "Green"
+
+		time = tableIndex*10
+		dispTable.update({tableIndex : [time,totalVehicles1,totalVehicles2,mesasge1,mesasge2]})
+
+		context = {'url1':url1,'url2':url2,'dispTable':dispTable,'signal1':mesasge1,'signal2':mesasge2}
+
+		return render(request,'app/trafficSimulation.html',context)
 
 # Create your views here.
